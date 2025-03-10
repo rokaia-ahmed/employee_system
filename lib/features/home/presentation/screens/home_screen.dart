806fa00ceaf67b3d038526d97044bc3ab293f600
rokia <rokiaahmed42@gmail.com>
2025-03-10 14:52:54 +0200

    home1

--- lib/features/home/presentation/screens/home_screen.dart
+++ lib/features/home/presentation/screens/home_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import '../../../../core/utils/styles.dart';
 import '../widgets/home_calender_date.dart';
 import '../widgets/home_profile_info.dart';
 
@@ -12,12 +13,28 @@ class HomeScreen extends StatelessWidget {
           child: Padding(
         padding: const EdgeInsets.fromLTRB(10, 20, 10, 10),
         child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
           children: [
             HomeProfileInfo(),
             SizedBox(
               height: 20,
             ),
             HomeCalenderDate(),
+            SizedBox(
+              height: 20,
+            ),
+            Text('Today attendance',
+               style: TextStyles.font18BlackW500,
+            ),
+            GridView.builder(
+              gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                  crossAxisCount: 2,
+              ),
+              itemBuilder: (context,index) =>Container(
+
+              ),
+
+            ),
           ],
         ),
       )),
